6dd1d64779b6be95524bf2cdfcc177de16fe568e
Gorstak-Zadar <goran.stambuk@mail2jesus.com>
2025-07-16 13:18:58 +0200

    Add files via upload

--- Perms.cmd
+++ Perms.cmd
@@ -4,6 +4,6 @@ icacls %windir%\System32\Oobe\useroobe.dll /inheritance:r
 icacls "%systemdrive%\Users" /remove "Everyone"
 icacls "C:\Users\Public" /reset /T
 icacls "%USERPROFILE%\Desktop" /reset /T
-icacls "%USERPROFILE%\" /setowner "%username%" /t /c /l
-icacls "%USERPROFILE%\" /remove "System" /t /c /l
-icacls "%USERPROFILE%\" /remove "Administrators" /t /c /l
+icacls "%USERPROFILE%\Desktop" /setowner "%username%" /t /c /l
+icacls "%USERPROFILE%\Desktop" /remove "System" /t /c /l
+icacls "%USERPROFILE%\Desktop" /remove "Administrators" /t /c /l
